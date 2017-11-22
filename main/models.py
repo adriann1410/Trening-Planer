d23fca24f514648fed6873c3c63c851ae2109a3d
adriann1410 <adriann11@interia.eu>
2017-11-22 17:07:48 +0100

    wstepne modele userow

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class CoachProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500)
+    rate = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class NormalProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.user.username
