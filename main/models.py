a376b5455c26a5ad5477d098fa6bec310b1b2068
adriann1410 <adriann11@interia.eu>
2017-11-23 15:48:34 +0100

    naprawilem komentaż

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ class CoachProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     rate = models.FloatField(default=0)
-    img_url = models.CharField(max_length=250, default="./static/img/default_user.png") //do zmiany przy uploadowaniu wlasnych plikow
+    img_url = models.CharField(max_length=250, default="./static/img/default_user.png") #do zmiany przy uploadowaniu wlasnych plikow
 
     def __str__(self):
         return self.user.username
