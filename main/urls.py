22a65d94aae5a508178589a2d1d9df705ff554f0
Michał Dróżdż <mich0232@gmail.com>
2017-11-23 15:27:12 +0100

    Register system init

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^login$', views.loginView, name="login"),
     url(r'^logout$', views.logoutView, name="logout"),
+    url(r'^register$', views.registerView, name="register"),
     url(r'^users$', views.users, name="users")
 ]
