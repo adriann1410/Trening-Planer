2607bdf477c4b885b4fc18fdf65b7d6965ea732c
adriann1410 <adriann11@interia.eu>
2017-11-23 15:03:09 +0100

    models extensions on admin page

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import CoachProfile, NormalProfile
 # Register your models here.
+
+admin.site.register(CoachProfile)
+admin.site.register(NormalProfile)
