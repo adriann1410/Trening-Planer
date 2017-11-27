d02e60aa5a1a4b2bb8bac4f0a2032129bf190c83
Adrian Jarosiński <adriann11@interia.eu>
2017-11-27 22:48:36 +0100

    models class like on ERD added

--- main/models.py
+++ main/models.py
@@ -17,6 +17,47 @@ class CoachProfile(models.Model):
 
 class NormalProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    height = models.IntegerField()
+    weight = models.IntegerField()
 
     def __str__(self):
         return  self.user.username
+
+
+class Pupil(models.Model):
+    coach_id = models.ManyToOneRel(CoachProfile)
+    pupil_id = models.OneToOneField(NormalProfile)
+
+    def __str__(self):
+        return self.id
+
+class Training(models.Model):
+    user = models.ManyToOneRel(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    exercises_num = models.IntegerField(default=0)
+    b_weight = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.id
+
+
+class Exercise(models.Model):
+    training = models.ManyToOneRel(Training, on_delete=models.CASCADE)
+    exercise_name = models.CharField(max_length=100)
+    series = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.exercise_name
+
+
+class Message(models.Model):
+    author = models.ManyToOneRel(User)
+    receiver = models.ManyToOneRel(User)
+    content = models.TextField(max_length=500)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.id
+
